bec8dc90a310ad4312ca588b2d416c9dc5f833aa
rok6 <sechs06re.ryo+glbd@gmail.com>
2017-01-31 06:39:35 +0900

    update: toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -3,25 +3,38 @@
 
   var $headers = $('article[id*=post-]').find(':header:not(h1)');
   var depth = 0,
-      current_level = 0,
+      head_level = 0,
       count = [0,0,0,0,0];
 
+  var max_depth = 2;
+
   $headers.each(function(i){
-    var num = this.tagName.match(/[2-6]/g);
 
+    var num = this.tagName.match(/[2-6]/g)[0];
 
-    if( current_level < num ) {
-      current_level = num;
-      count[depth]++;
+    if( head_level == num ) {
+
+    }
+    else if( head_level < num ) {
+      head_level = num;
       depth++;
     }
-    else if( current_level > num ) {
-      current_level = num;
-      count[depth] = 0;
-      depth--;
+    else if( head_level > num ) {
+      head_level = num;
+      while( head_level <= depth ) {
+        count[depth - 1] = 0;
+        depth--;
+      }
     }
-    console.log(count);
-    $(this).attr({id, 'section' + count[0] + count[1]});
+    count[depth - 1]++;
+
+    if( depth <= max_depth ) {
+      var anchor = 'section'+ count[0];
+      for( var j = 0; j < depth - 1; j++ ) {
+        anchor += '-' + count[j+1];
+      }
+      $(this).attr({'id': anchor});
+    }
 
   });
 
